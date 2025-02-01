90336178d6c1713b937dee7bcecfa3dd7e10194a
nk-on <2nkoz@proton.me>
2025-01-31 21:58:17 -0800

    Feat:Adding cover photo inside heading and switching between night and day modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import Heading from "./components/Heading";
 function App() {
-
+  const [nightMode, setNightMode] = useState(false);
   return (
     <>
-     
+        <div className={`w-[100%] ${nightMode ? 'dark':''}`}>
+          <Heading nightMode = {nightMode} setNightMode={setNightMode} />
+        </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Heading.tsx
@@ -0,0 +1,23 @@
+interface HeadingProps {
+  nightMode: boolean;
+  setNightMode: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export default function Heading({ nightMode, setNightMode }: HeadingProps) {
+  // const coverImage = nightMode ? 'public/assets/cover-photo-night.jpg':'./assets/cover-photo-day.jpg';
+  return (
+    <div className="w-[100%] h-[50vh] bg-no-repeat  bg-[url(./assets/cover-photo-day.jpg)] dark:bg-[url(./assets/cover-photo-night.jpg)] flex justify-center items-center">
+      <div className="flex w-[50%] justify-between">
+        <h1 className="text-[#FFFFFF] font-bold text-[40px] text-[26px] tracking-[0.5em] uppercase">Todo</h1>
+        <img
+          className="cursor-pointer dark:"
+          onClick={()=>{
+            setNightMode(!nightMode)
+          }}
+          src={`${
+            nightMode ? "public/assets/icon-night.svg" : "public/assets/icon-day-sun.svg"
+          }`}
+        ></img>
+      </div>
+    </div>
+  );
+}
