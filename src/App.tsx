2e9b93fa8eb6ff2bc487e75efee96409d1a9d904
nk-on <johndoe@example.com>
2025-02-03 22:58:15 +0400

    Style:adding dark mode to the body

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import "./App.css";
-import Heading from "./components/Heading";
-import TodoList from "./components/TodoList";
-import { createContext } from "react";
-export const nightModeContext = createContext(false)
+import { useState } from 'react';
+import './App.css';
+import Heading from './components/Heading';
+import TodoList from './components/TodoList';
+import { createContext } from 'react';
+export const nightModeContext = createContext(false);
 function App() {
   const [nightMode, setNightMode] = useState(false);
   return (
     <>
-        <div className={`w-[100%] ${nightMode ? 'dark':''}`}>
-          <Heading nightMode = {nightMode} setNightMode={setNightMode} />
+      <div className= {`w-[100%] h-[100vh] ${nightMode && 'bg-[#171823]'} ${nightMode ? 'dark' : ''}`} >
+          <Heading nightMode={nightMode} setNightMode={setNightMode} />
           <TodoList />
-        </div>
+      </div>
     </>
   );
 }
