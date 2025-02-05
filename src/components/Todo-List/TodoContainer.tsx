0fa45e1dbb82601cfbd1e30afa7dd4455dc5a543
nk-on <2nkoz@proton.me>
2025-02-04 21:45:40 -0800

    Style:adding and styling bottom section of container

--- src/components/Todo-List/TodoContainer.tsx
+++ src/components/Todo-List/TodoContainer.tsx
@@ -1,5 +1,15 @@
-export default function TodoContainer(){
-    return (
-        <div className="w-[100%] h-[439px] shadow-[0px_35px_50px_-15px_#C2C3D680] bg-[#FFFFFF] dark:bg-[#25273D] dark:shadow-[0px_35px_50px_-15px_#00000080]"></div>
-    )
+import Todo from "./Todo";
+export default function TodoContainer() {
+  return (
+    <div className="w-[100%] shadow-[0px_35px_50px_-15px_#C2C3D680] bg-[#FFFFFF] dark:bg-[#25273D] dark:shadow-[0px_35px_50px_-15px_#00000080]">
+      <Todo />
+      <div className="w-[100%] border-b-2 border-[#E3E4F1] text-[#9495A5] p-4 flex justify-between">
+        <div>5 items left</div>
+        <div className="font-bold hover:text-[#494C6B] cursor-pointer">All</div>
+        <div className="font-bold  hover:text-[#494C6B]  cursor-pointer">Active</div>
+        <div className="font-bold  hover:text-[#494C6B]  cursor-pointer">Completed</div>
+        <div>Clear Completed</div>
+      </div>
+    </div>
+  );
 }
