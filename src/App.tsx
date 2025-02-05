86f0758afe94c7fe53352c1e44e595685f05280a
nk-on <2nkoz@proton.me>
2025-02-04 21:33:18 -0800

    Adding defining min height inside App div

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import Heading from './components/Heading';
 import TodoList from './components/TodoList';
-import { createContext } from 'react';
-export const nightModeContext = createContext(false);
 function App() {
   const [nightMode, setNightMode] = useState(false);
   return (
     <>
-      <div className= {`w-[100%] h-[100vh] ${nightMode && 'bg-[#171823]'} ${nightMode ? 'dark' : ''}`} >
+      <div className= {`w-[100%] min-h-[100vh] ${nightMode && 'bg-[#171823]'} ${nightMode ? 'dark' : ''}`} >
           <Heading nightMode={nightMode} setNightMode={setNightMode} />
           <TodoList />
       </div>
