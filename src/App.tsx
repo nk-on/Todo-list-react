18e004491550a75645a30af7b6e65d910a1c0db9
nk-on <2nkoz@proton.me>
2025-02-05 21:35:50 -0800

    Feat:creating array with individual todo item objects

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,30 @@ import { useState } from 'react';
 import './App.css';
 import Heading from './components/Heading';
 import TodoList from './components/TodoList';
+import { createContext } from 'react';
+interface Task {
+  id: number;
+  name: string;
+  completed: boolean;
+}
+interface contextProps {
+  Todos:Task[],
+  SetTodos:React.Dispatch<React.SetStateAction<Task[]>>,
+}
+export const TodosContext = createContext<contextProps>({
+  Todos:[],
+  SetTodos:() => {},
+});
 function App() {
   const [nightMode, setNightMode] = useState(false);
+  const [Todos,SetTodos] = useState<Task[]>([])
   return (
     <>
       <div className= {`w-[100%] min-h-[100vh] ${nightMode && 'bg-[#171823]'} ${nightMode ? 'dark' : ''}`} >
           <Heading nightMode={nightMode} setNightMode={setNightMode} />
-          <TodoList />
+          <TodosContext.Provider value={{Todos,SetTodos}}>
+            <TodoList />
+          </TodosContext.Provider>
       </div>
     </>
   );
--- src/components/Todo-List/TodoInput.tsx
+++ src/components/Todo-List/TodoInput.tsx
@@ -1,10 +1,35 @@
+import { useRef } from "react";
+import { useContext } from "react";
+import { TodosContext } from "../../App";
 export default function TodoInput() {
+  const {Todos,SetTodos} = useContext(TodosContext);
+  const task = useRef<string>("");
   return (
     <div className="w-[100%]  bg-[#FFFFFF] dark:bg-[#25273D] flex justify-between rounded-[5px]">
       <div className="flex justify-center items-center w-[5%]">
-        <input type="radio" className="w-[24px] h-[24px] dark:bg-[#25273D]" />
+        <input
+          type="radio"
+          className="w-[24px] h-[24px] dark:bg-[#25273D]"
+          onChange={(event) => {
+            const Todo = {
+              id:Math.floor(Math.random()*100000),
+              name:task.current,
+              completed:false,
+            }
+            SetTodos([...Todos,Todo])
+            event.target.checked = false;
+          }}
+        />
       </div>
-      <input type="text" className="w-[90%] h-[64px] dark:bg-[#25273D] dark:text-[#FFFFFF] placeholder:text-lg" placeholder="Create a new Todo"/>;
+      <input
+        type="text"
+        className="w-[90%] h-[64px] dark:bg-[#25273D] dark:text-[#FFFFFF] placeholder:text-lg"
+        placeholder="Create a new Todo"
+        onChange={(event)=>{
+          task.current = event.target.value;
+        }}
+      />
+      ;
     </div>
   );
 }
