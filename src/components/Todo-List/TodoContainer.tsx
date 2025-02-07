f36ae33724d32c3a24f7ec4f3a2175f3e7f3bd80
nk-on <2nkoz@proton.me>
2025-02-06 21:18:55 -0800

    Feat:Adding feature which lets user to leave only completed tasks

--- src/components/Todo-List/TodoContainer.tsx
+++ src/components/Todo-List/TodoContainer.tsx
@@ -2,7 +2,7 @@ import Todo from "./Todo";
 import { useContext } from "react";
 import { TodosContext } from "../../Context";
 export default function TodoContainer() {
-  const {Todos} = useContext(TodosContext)
+  const {Todos,SetTodos} = useContext(TodosContext)
   return (
     <div className="w-[100%] shadow-[0px_35px_50px_-15px_#C2C3D680] bg-[#FFFFFF] dark:bg-[#25273D] dark:shadow-[0px_35px_50px_-15px_#00000080]">
       {
@@ -14,7 +14,13 @@ export default function TodoContainer() {
         <div>5 items left</div>
         <div className="font-bold hover:text-[#494C6B] cursor-pointer">All</div>
         <div className="font-bold  hover:text-[#494C6B]  cursor-pointer">Active</div>
-        <div className="font-bold  hover:text-[#494C6B]  cursor-pointer">Completed</div>
+        <div className="font-bold  hover:text-[#494C6B]  cursor-pointer" onClick={()=>{
+          SetTodos(prevObject => 
+            prevObject.filter((todoItem)=>{
+              return todoItem.completed;
+            })
+          )
+        }}>Completed</div>
         <div>Clear Completed</div>
       </div>
     </div>
