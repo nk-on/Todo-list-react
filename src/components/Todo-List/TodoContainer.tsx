ceed38c4e8a929203cd98b2b86a1882067d9d78c
nk-on <2nkoz@proton.me>
2025-02-06 21:31:27 -0800

    Feat:Adding feature which lets users to leave only incompleted tasks on the board

--- src/components/Todo-List/TodoContainer.tsx
+++ src/components/Todo-List/TodoContainer.tsx
@@ -1,8 +1,10 @@
 import Todo from "./Todo";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { TodosContext } from "../../Context";
+import { Task } from "../../App";
 export default function TodoContainer() {
-  const {Todos,SetTodos} = useContext(TodosContext)
+  const {Todos,SetTodos} = useContext(TodosContext);
+  const originalTodos = useRef<Task[]>([])
   return (
     <div className="w-[100%] shadow-[0px_35px_50px_-15px_#C2C3D680] bg-[#FFFFFF] dark:bg-[#25273D] dark:shadow-[0px_35px_50px_-15px_#00000080]">
       {
@@ -12,9 +14,17 @@ export default function TodoContainer() {
       }
       <div className="w-[100%] border-b-2 border-[#E3E4F1] text-[#9495A5] p-4 flex justify-between">
         <div>5 items left</div>
-        <div className="font-bold hover:text-[#494C6B] cursor-pointer">All</div>
-        <div className="font-bold  hover:text-[#494C6B]  cursor-pointer">Active</div>
+        <div className="font-bold hover:text-[#494C6B] cursor-pointer" onClick={()=>{
+          SetTodos(originalTodos.current)
+        }}>All</div>
         <div className="font-bold  hover:text-[#494C6B]  cursor-pointer" onClick={()=>{
+          originalTodos.current = [...Todos];
+          SetTodos(prevObject => prevObject.filter((todoItem)=>{
+            return !todoItem.completed;
+          }))
+        }}>Active</div>
+        <div className="font-bold  hover:text-[#494C6B]  cursor-pointer" onClick={()=>{
+          originalTodos.current = [...Todos]
           SetTodos(prevObject => 
             prevObject.filter((todoItem)=>{
               return todoItem.completed;
