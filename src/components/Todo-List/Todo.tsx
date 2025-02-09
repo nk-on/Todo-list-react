b2c8418157994520fcd726819ee59e616d73c3f0
nk-on <2nkoz@proton.me>
2025-02-08 21:15:58 -0800

    Refactor:activating behaviour which adds checked status on todo item when clicking text instead of clicking on the radio input

--- src/components/Todo-List/Todo.tsx
+++ src/components/Todo-List/Todo.tsx
@@ -1,22 +1,36 @@
-import { TodosContext } from "../../Context"
-import { useContext } from "react"
+import { TodosContext } from "../../Context";
+import { useContext } from "react";
 interface TaskProps {
-  taskName:string,
-  completed:boolean
-  id:number
+  taskName: string;
+  completed: boolean;
+  id: number;
 }
-export default function Todo({taskName,completed,id}:TaskProps) {
-  const {SetTodos} = useContext(TodosContext);
+export default function Todo({ taskName, completed, id }: TaskProps) {
+  const { SetTodos } = useContext(TodosContext);
   return (
     <div className="w-[100%] border-b-2 border-[#E3E4F1] p-4">
       <div className="flex justify-between w-[20%]">
-        <input type="radio" className="w-[24px] h-[24px] dark:bg-[#25273D]" onChange={(event)=>{
-          SetTodos(prevObjects => prevObjects.map((todoItem)=>{
-            return todoItem.id === id ? {...todoItem,completed:!completed}:todoItem;
-          }))
-          event.target.checked = false;
-        }}/>
-        <p className={`text-[18px dark:text-[#C8CBE7] ${completed && "line-through"}`}>{taskName}</p>
+        <input
+          type="radio"
+          className="w-[24px] h-[24px] dark:bg-[#25273D]"
+          onChange={(event) => {
+            event.target.checked = false;
+          }}
+        />
+        <p
+          className={`text-[18px dark:text-[#C8CBE7] ${completed && "line-through"} cursor-pointer`}
+          onClick={() => {
+            SetTodos((prevObjects) =>
+              prevObjects.map((todoItem) => {
+                return todoItem.id === id
+                  ? { ...todoItem, completed: !completed }
+                  : todoItem;
+              })
+            );
+          }}
+        >
+          {taskName}
+        </p>
       </div>
     </div>
   );
