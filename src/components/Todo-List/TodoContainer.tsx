c1d751446cb38a2a8128d15f2c86f7b0ff17db12
nk-on <2nkoz@proton.me>
2025-02-08 21:28:45 -0800

    Feat:adding feature which will display numbers of exsiting todo items

--- src/components/Todo-List/TodoContainer.tsx
+++ src/components/Todo-List/TodoContainer.tsx
@@ -50,7 +50,7 @@ export default function TodoContainer() {
     <div className="w-[100%] shadow-[0px_35px_50px_-15px_#C2C3D680] bg-[#FFFFFF] dark:bg-[#25273D] dark:shadow-[0px_35px_50px_-15px_#00000080]">
       {renderTodos()}
       <div className="w-[100%] border-b-2 border-[#E3E4F1] text-[#9495A5] p-4 flex justify-between">
-        <div>5 items left</div>
+        <div>{Todos.length} items left</div>
         <div
           className="font-bold hover:text-[#494C6B] cursor-pointer dark:hover:text-[#FFFFFF]"
           onClick={() => {
