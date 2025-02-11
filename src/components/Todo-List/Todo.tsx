b087fb4ac571c8da499adec40626faa47bbbe76b
nk-on <2nkoz@proton.me>
2025-02-10 21:41:14 -0800

    Feat:adding background and check symbol when clicking on checkbox

--- src/components/Todo-List/Todo.tsx
+++ src/components/Todo-List/Todo.tsx
@@ -1,30 +1,19 @@
 import { TodosContext } from "../../Context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 interface TaskProps {
   taskName: string;
   completed: boolean;
   id: number;
 }
 export default function Todo({ taskName, completed, id }: TaskProps) {
-  const {Todos, SetTodos } = useContext(TodosContext);
+  const { Todos, SetTodos } = useContext(TodosContext);
   return (
     <div className="w-[100%] border-b-2 border-[#E3E4F1] p-4">
       <div className="flex justify-between w-[20%]">
-        <input
-          type="radio"
-          className="w-[24px] h-[24px] dark:bg-[#25273D]"
-          onChange={(event) => {
-            const filteredTodos = Todos.filter((todoItem)=>{
-              if(id !== todoItem.id){
-                return todoItem;
-              }
-            });
-            SetTodos(filteredTodos);
-            event.target.checked = false;
-          }}
-        />
-        <p
-          className={`text-[18px dark:text-[#C8CBE7] ${completed && "line-through"} cursor-pointer`}
+        <div
+          className={`flex justify-center items-center w-[24px] h-[24px] rounded-[50%] border border-[#E3E4F1] dark:bg-[#25273D] cursor-pointer  ${
+            completed && "bg-gradient-to-br from-[#55DDFF] to-[#C058F3]"
+          }`}
           onClick={() => {
             SetTodos((prevObjects) =>
               prevObjects.map((todoItem) => {
@@ -34,6 +23,12 @@ export default function Todo({ taskName, completed, id }: TaskProps) {
               })
             );
           }}
+        >{completed && <img src="public/assets/Checked.svg" alt="checked" />}</div>
+        <p
+          className={`text-[18px dark:text-[#C8CBE7] ${
+            completed && "line-through"
+          } cursor-pointer`}
+          onClick={() => {}}
         >
           {taskName}
         </p>
