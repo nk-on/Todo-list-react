2f973ef8665707849e9c87a15fe172abf2da44ad
nk-on <2nkoz@proton.me>
2025-02-10 21:53:00 -0800

    Feat:Adding closing sign and when user clicks it deleting individual todo item

--- src/components/Todo-List/Todo.tsx
+++ src/components/Todo-List/Todo.tsx
@@ -7,8 +7,13 @@ interface TaskProps {
 }
 export default function Todo({ taskName, completed, id }: TaskProps) {
   const { Todos, SetTodos } = useContext(TodosContext);
+  const [hovered, setHovered] = useState<boolean>(false);
   return (
-    <div className="w-[100%] border-b-2 border-[#E3E4F1] p-4">
+    <div
+      className="w-[100%] border-b-2 border-[#E3E4F1] p-4 flex justify-between"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
       <div className="flex justify-between w-[20%]">
         <div
           className={`flex justify-center items-center w-[24px] h-[24px] rounded-[50%] border border-[#E3E4F1] dark:bg-[#25273D] cursor-pointer  ${
@@ -23,16 +28,25 @@ export default function Todo({ taskName, completed, id }: TaskProps) {
               })
             );
           }}
-        >{completed && <img src="public/assets/Checked.svg" alt="checked" />}</div>
+        >
+          {completed && <img src="public/assets/Checked.svg" alt="checked" />}
+        </div>
         <p
           className={`text-[18px dark:text-[#C8CBE7] ${
             completed && "line-through"
           } cursor-pointer`}
-          onClick={() => {}}
         >
           {taskName}
         </p>
       </div>
+      {hovered && <img src={"public/assets/Delete.svg"} className = "cursor-pointer" alt="closeSign" onClick={()=>{
+        const filteredTodos = Todos.filter((todoItem)=>{
+          if(todoItem.id !== id){
+            return todoItem;
+          }
+        })
+        SetTodos(filteredTodos);
+      }} />}
     </div>
   );
 }
