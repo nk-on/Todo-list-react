e07aa285bb16faa579b5ef5698f67474c0633a0b
nk-on <2nkoz@proton.me>
2025-02-12 21:23:34 -0800

    Refactor:coding checkboxes as a separete componenets

---
+++ src/components/CheckButton.tsx
@@ -0,0 +1,18 @@
+interface CheckButtonProp  {
+    completed:boolean;
+    functionProp: () => void;
+}
+export function CheckButton({completed,functionProp}:CheckButtonProp) {
+  return (
+    <div
+      className={`flex justify-center items-center w-[24px] h-[24px] rounded-[50%] border border-[#E3E4F1] dark:bg-[#25273D] cursor-pointer  ${
+        completed && "bg-gradient-to-br from-[#55DDFF] to-[#C058F3]"
+      }`}
+      onClick={() => {
+        functionProp();
+      }}
+    >
+      {completed && <img src="./assets/Checked.svg" alt="checked" />}
+    </div>
+  );
+}
