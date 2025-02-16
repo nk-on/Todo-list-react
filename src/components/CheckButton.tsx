3f9f9a7c4dd344932cb5bf212d855fba4ac022e5
nk-on <2nkoz@proton.me>
2025-02-15 22:33:58 -0800

    src/components/CheckButton/tsx

--- src/components/CheckButton.tsx
+++ src/components/CheckButton.tsx
@@ -8,9 +8,7 @@ export function CheckButton({completed,functionProp}:CheckButtonProp) {
       className={`flex justify-center items-center w-[24px] h-[24px] rounded-[50%] border border-[#E3E4F1] dark:bg-[#25273D] cursor-pointer  ${
         completed && "bg-gradient-to-br from-[#55DDFF] to-[#C058F3]"
       }`}
-      onClick={() => {
-        functionProp();
-      }}
+      onClick={functionProp}
     >
       {completed && <img src="./assets/Checked.svg" alt="checked" />}
     </div>
