b1d89a151afdf15efd851b6fa03ba8d598fbadc2
nk-on <2nkoz@proton.me>
2025-02-16 07:52:48 -0800

    Refactor:Filtering Todos based on completed criteria instead of mapping based on string values

--- src/components/Todo-List/TodoContainer.tsx
+++ src/components/Todo-List/TodoContainer.tsx
@@ -1,64 +1,40 @@
 import Todo from "./Todo";
-import { useContext ,useState } from "react";
+import { useContext, useState } from "react";
 import { TodosContext } from "../Context";
 import { FilterOption } from "../filterOption";
 export default function TodoContainer() {
-  const { Todos,SetTodos } = useContext(TodosContext);
-  const [filterCriteria, setFilterCriteria] = useState<string>("All");
-  const filterCriteriaArray = ["All","Completed","Active"]
+  const { Todos, SetTodos } = useContext(TodosContext);
+  const [filterCriteria, setFilterCriteria] = useState<boolean | string>("All");
+  const filterCriteriaArray = ["All", "Completed", "Active"];
   const renderTodos = () => {
-    if (filterCriteria === "All") {
-      return Todos.map((todoItem) => {
-        return (
-          <Todo
-            key={todoItem.id}
-            taskName={todoItem.name}
-            completed={todoItem.completed}
-            id={todoItem.id}
-          />
-        );
-      });
-    }
-    if(filterCriteria === 'Completed'){
-      return Todos.map((todoItem) => {
-        if(todoItem.completed){
-          return (
-            <Todo
-              key={todoItem.id}
-              taskName={todoItem.name}
-              completed={todoItem.completed}
-              id={todoItem.id}
-            />
-          );
-        }
-      });
-    }
-    if(filterCriteria === 'Active'){
-      return Todos.map((todoItem) => {
-        if(!todoItem.completed){
-          return (
-            <Todo
-              key={todoItem.id}
-              taskName={todoItem.name}
-              completed={todoItem.completed}
-              id={todoItem.id}
-            />
-          );
-        }
-      });
-    }
+    const filteredTodos = filterCriteria === "All" ? Todos : Todos.filter((todo) => todo.completed === filterCriteria);
+    return filteredTodos.map((todoItem) => {
+      return (
+        <Todo
+          key={todoItem.id}
+          taskName={todoItem.name}
+          completed={todoItem.completed}
+          id={todoItem.id}
+        />
+      );
+    });
   };
   return (
     <div className="w-[100%] shadow-[0px_35px_50px_-15px_#C2C3D680] bg-[#FFFFFF] dark:bg-[#25273D] dark:shadow-[0px_35px_50px_-15px_#00000080]">
       {renderTodos()}
       <div className="w-[100%] border-b-2 border-[#E3E4F1] text-[#9495A5] p-4 flex justify-between">
         <div>{Todos.length} items left</div>
-        {
-          filterCriteriaArray.map(item=> <FilterOption text={item} setFilterCriteria={setFilterCriteria} />)
-        }
-        <div className="cursor-pointer" onClick={()=>{
-          SetTodos([]);
-        }}>Clear Completed</div>
+        {filterCriteriaArray.map((item) => (
+          <FilterOption text={item} setFilterCriteria={setFilterCriteria} />
+        ))}
+        <div
+          className="cursor-pointer"
+          onClick={() => {
+            SetTodos([]);
+          }}
+        >
+          Clear Completed
+        </div>
       </div>
     </div>
   );
--- src/components/filterOption.tsx
+++ src/components/filterOption.tsx
@@ -1,13 +1,13 @@
 interface FilterOptionProps  {
     text:string,
-    setFilterCriteria: React.Dispatch<React.SetStateAction<string>>
+    setFilterCriteria: React.Dispatch<React.SetStateAction<boolean | string>>
 }
 export function FilterOption({text,setFilterCriteria}:FilterOptionProps){
     return (
         <div
         className="font-bold  hover:text-[#494C6B]  cursor-pointer dark:hover:text-[#FFFFFF]"
         onClick={() => {
-          setFilterCriteria(text);
+          setFilterCriteria(text === "Completed" ? true : false);
         }}
       >
         {text}
